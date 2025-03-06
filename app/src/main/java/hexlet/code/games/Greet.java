03ac9e8f68d84480c40f024b926a78efb65d3c0f
Lakever <givster@yandex.ru>
2025-03-06 18:35:19 +0300

    Update Greet.java

--- app/src/main/java/hexlet/code/games/Greet.java
+++ app/src/main/java/hexlet/code/games/Greet.java
@@ -13,7 +13,7 @@ public class Greet {
         System.out.println("Welcome to the Brain Games!");
         System.out.println("May I have your name?");
         person = Cli.greetings();
-        System.out.printf("Hello, " + person + "!");
+        System.out.println("Hello, " + person + "!");
         return person;
 
 
