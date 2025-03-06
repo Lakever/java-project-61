91b2ae1276bbac976f80c22b8a9da2ebfe77fd28
Lakever <givster@yandex.ru>
2025-03-06 19:21:58 +0300

    CorrectingError#3

--- app/src/main/java/hexlet/code/games/Eval.java
+++ app/src/main/java/hexlet/code/games/Eval.java
@@ -21,24 +21,32 @@ public class Eval {
         int countCorrect = 0;
         for (int i = 0; i < 3; i++) {
             int randomNumber = random.nextInt(100);
+
             System.out.println("Question: " + randomNumber);
             System.out.println("Your answer: ");
             String answer = s.nextLine();
+
             if (answer.equals("yes") && randomNumber % 2 == 0) {
                 System.out.println("Correct!");
                 countCorrect++;
+                if (countCorrect == 3) {
+                    System.out.println("Congratulations, " + person);
+                    return "Congratulations, " + person;
+                }
             } else if (answer.equals("no") && randomNumber % 2 != 0) {
                 countCorrect++;
                 System.out.println("Correct!");
+                if (countCorrect == 3) {
+                    System.out.println("Congratulations, " + person);
+                    return "Congratulations, " + person;
+                }
             } else {
                 System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'."
                         + " Let's try again, " + person);
                 break;
             }
         }
-        if (countCorrect == 3) {
-            return "Congratulations, " + person;
-        }
-        return "";
+        return  "";
+
     }
 }
