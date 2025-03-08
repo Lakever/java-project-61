7b0cbdec95023b6aab9a55b143e405619c945c24
Lakever <givster@yandex.ru>
2025-03-08 18:53:13 +0300

    corrErr15

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -8,40 +8,52 @@ import static hexlet.code.games.Greet.startGreet;
 public class Prime {
     public static void startGamePrime() {
         var person = startGreet(6);
-
-        Random rand = new Random();
-        Scanner scanner = new Scanner(System.in);
+        int count = 0;
         System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
 
-        int randNum = rand.nextInt(100) + 1;
-        System.out.println("Question: " + randNum);
+        for (var j = 0; j < 3; j ++) {
+            Random rand = new Random();
+            Scanner scanner = new Scanner(System.in);
 
-        String answer = scanner.nextLine();
-        String status = "";
 
-        if (randNum < 2) {
-            status = "no"; // 0 и 1 не являются простыми числами
-        } else if (randNum == 2 || randNum == 3) {
-            status = "yes"; // 2 и 3 — простые числа
-        } else if (randNum % 2 == 0 || randNum % 3 == 0) {
-            status = "no"; // Числа, делящиеся на 2 или 3, не являются простыми
-        } else {
-            status = "yes"; // Предполагаем, что число простое, пока не найдём делитель
-            for (int i = 5; i * i <= randNum; i += 6) {
-                if (randNum % i == 0 || randNum % (i + 2) == 0) {
-                    status = "no"; // Если делитель найден, число не простое
-                    break; // Прерываем цикл, так как дальнейшая проверка не нужна
+            int randNum = rand.nextInt(100) + 1;
+            System.out.println("Question: " + randNum);
+
+            String answer = scanner.nextLine();
+            String status = "";
+
+            if (randNum < 2) {
+                status = "no"; // 0 и 1 не являются простыми числами
+            } else if (randNum == 2 || randNum == 3) {
+                status = "yes"; // 2 и 3 — простые числа
+            } else if (randNum % 2 == 0 || randNum % 3 == 0) {
+                status = "no"; // Числа, делящиеся на 2 или 3, не являются простыми
+            } else {
+                status = "yes"; // Предполагаем, что число простое, пока не найдём делитель
+                for (int i = 5; i * i <= randNum; i += 6) {
+                    if (randNum % i == 0 || randNum % (i + 2) == 0) {
+                        status = "no"; // Если делитель найден, число не простое
+                        break; // Прерываем цикл, так как дальнейшая проверка не нужна
+                    }
                 }
             }
+
+            if (answer.equalsIgnoreCase(status)) {
+                System.out.printf("Your answer: '%s'\n", answer);
+                System.out.println("Correct!");
+                count++;
+                if (count == 3) {
+                    System.out.println("Congratulations, " + person + "!");
+                }
+            } else {
+                System.out.println("'" + answer + "'" + " is wrong answer ;(. "
+                        + "Correct answer was " + "'" + status + "'" + ".");
+                System.out.println("Let's try again, " + person + "!");
+                break;
+            }
+
         }
 
-        if (answer.equalsIgnoreCase(status)) {
-            System.out.printf("Your answer: '%s'\n", answer);
-            System.out.println("Correct!");
-        } else {
-            System.out.println("'" + answer + "'" + " is wrong answer ;(. "
-                    + "Correct answer was " + "'" + status + "'" + ".");
-            System.out.println("Let's try again, " + person + "!");
-        }
+
     }
 }
