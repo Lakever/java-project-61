f51b98bfddfd73a90294b363d28a9004a2ebb8c1
Lakever <givster@yandex.ru>
2025-03-10 18:14:22 +0300

    refactoring

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,12 +2,65 @@
 package hexlet.code;
 
 
-import static hexlet.code.Engine.startEngine;
+import hexlet.code.games.*;
+
+import java.util.Scanner;
 
 
 public class App {
+    private static final int EXIT_GAME = 0;
+    private static final int GREET_GAME = 1;
+    private static final int EVAL_GAME = 2;
+    private static final int CALC_GAME = 3;
+    private static final int GCD_GAME = 4;
+    private static final int PROGRESSION_GAME = 5;
+    private static final int PRIME_GAME = 6;
+
     public static void main(String[] args) {
-        var start = new Engine();
+
+
+        // Именованные константы для выбора игры
+
+
+
+
+
+
         startEngine();
     }
+
+    public static void startEngine () {
+        System.out.println("Please enter the game number and press Enter.");
+        var menu = new Menu();
+        menu.showMenu();
+        Scanner input = new Scanner(System.in);
+        int num = Integer.parseInt(input.nextLine());
+
+        switch (num) {
+            case EXIT_GAME:
+                System.out.println("Goodbye!");
+                break;
+            case GREET_GAME:
+                Greet.startGreet(1);
+                break;
+            case EVAL_GAME:
+                Eval.startGameEval();
+                break;
+            case CALC_GAME:
+                Calc.startGameCalc();
+                break;
+            case GCD_GAME:
+                GCD.startGameGCD();
+                break;
+            case PROGRESSION_GAME:
+                Progression.startGameProgression();
+                break;
+            case PRIME_GAME:
+                Prime.startGamePrime();
+                break;
+            default:
+                System.out.println("Goodbye!");
+        }
+    }
+
 }
--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,55 +1,55 @@
-package hexlet.code;
-
-import hexlet.code.games.Calc;
-import hexlet.code.games.Eval;
-import hexlet.code.games.GCD;
-import hexlet.code.games.Greet;
-import hexlet.code.games.Prime;
-import hexlet.code.games.Progression;
-
-import java.util.Scanner;
-
-public class Engine {
-    // Именованные константы для выбора игры
-    private static final int EXIT_GAME = 0;
-    private static final int GREET_GAME = 1;
-    private static final int EVAL_GAME = 2;
-    private static final int CALC_GAME = 3;
-    private static final int GCD_GAME = 4;
-    private static final int PROGRESSION_GAME = 5;
-    private static final int PRIME_GAME = 6;
-
-    public static void startEngine() {
-        System.out.println("Please enter the game number and press Enter.");
-        var menu = new Menu();
-        menu.showMenu();
-        Scanner input = new Scanner(System.in);
-        int num = Integer.parseInt(input.nextLine());
-
-        switch (num) {
-            case EXIT_GAME:
-                System.out.println("Goodbye!");
-                break;
-            case GREET_GAME:
-                Greet.startGreet(1);
-                break;
-            case EVAL_GAME:
-                Eval.startGameEval();
-                break;
-            case CALC_GAME:
-                Calc.startGameCalc();
-                break;
-            case GCD_GAME:
-                GCD.startGameGCD();
-                break;
-            case PROGRESSION_GAME:
-                Progression.startGameProgression();
-                break;
-            case PRIME_GAME:
-                Prime.startGamePrime();
-                break;
-            default:
-                System.out.println("Goodbye!");
-        }
-    }
-}
+//package hexlet.code;
+//
+//import hexlet.code.games.Calc;
+//import hexlet.code.games.Eval;
+//import hexlet.code.games.GCD;
+//import hexlet.code.games.Greet;
+//import hexlet.code.games.Prime;
+//import hexlet.code.games.Progression;
+//
+//import java.util.Scanner;
+//
+//public class Engine {
+//    // Именованные константы для выбора игры
+//    private static final int EXIT_GAME = 0;
+//    private static final int GREET_GAME = 1;
+//    private static final int EVAL_GAME = 2;
+//    private static final int CALC_GAME = 3;
+//    private static final int GCD_GAME = 4;
+//    private static final int PROGRESSION_GAME = 5;
+//    private static final int PRIME_GAME = 6;
+//
+//    public static void startEngine() {
+//        System.out.println("Please enter the game number and press Enter.");
+//        var menu = new Menu();
+//        menu.showMenu();
+//        Scanner input = new Scanner(System.in);
+//        int num = Integer.parseInt(input.nextLine());
+//
+//        switch (num) {
+//            case EXIT_GAME:
+//                System.out.println("Goodbye!");
+//                break;
+//            case GREET_GAME:
+//                Greet.startGreet(1);
+//                break;
+//            case EVAL_GAME:
+//                Eval.startGameEval();
+//                break;
+//            case CALC_GAME:
+//                Calc.startGameCalc();
+//                break;
+//            case GCD_GAME:
+//                GCD.startGameGCD();
+//                break;
+//            case PROGRESSION_GAME:
+//                Progression.startGameProgression();
+//                break;
+//            case PRIME_GAME:
+//                Prime.startGamePrime();
+//                break;
+//            default:
+//                System.out.println("Goodbye!");
+//        }
+//    }
+//}
